a7fd1b910471f3b0f79977d73945b1803c151e6a
Veto1023404 <iamveto@outlook.com>
2016-03-01 00:43:49 +0000

    Azir 1.0

---
+++ Azir/Azir.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+namespace Azir
+{
+    class Azir
+    {
+        public static Obj_AI_Hero Player = ObjectManager.Player;
+
+        internal static void Load(EventArgs args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            if (Player.ChampionName != "Azir")
+                return;
+
+            Spells.Initialise();
+            MenuConfig.LoadMenu();
+
+            List<GameObject> Soldiers = SoldierManager.AzirSoldiers;
+
+            Game.OnUpdate += Game_OnUpdate;
+            Interrupter2.OnInterruptableTarget += Interrupter2_OnInterruptableTarget;
+            Drawing.OnDraw += Drawing_OnDraw;
+
+            Game.PrintChat("[00:00] Azir # By Veto");
+            Game.PrintChat("[00:00] Remember to Upvote in the Assembly Database!");
+        }
+
+        static void Game_OnUpdate(EventArgs args)
+        {
+            if (Player.IsDead || args == null)
+                return;
+            
+            switch (MenuConfig.Orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+                    Combo();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    Harass();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.LaneClear:
+                    Laneclear();
+                    break;
+            }
+
+            if (IsActive("Combo.All_In"))
+                AllInCombo();
+
+            if (IsActive("EliteCombo"))
+                EliteAzirCombo(Game.CursorPos);
+
+            if (IsActive("Escape"))
+                Escape(Game.CursorPos);
+
+            if (IsActive("DrawDamage"))
+                Utility.HpBarDamageIndicator.DamageToUnit += target => ComboDamage(target);
+        }
+
+        private static bool IsActive(string menuItem)
+        {
+            return MenuConfig.config.Item(menuItem).GetValue<KeyBind>().Active;
+        }
+        static void Combo()
+        {
+            var target = TargetSelector.GetTarget(Spells.Q.Range, TargetSelector.DamageType.Magical);
+
+            foreach (var enemy in HeroManager.Enemies.Where(x => x.IsValidTarget(Spells.Q.Range - 15)))
+            {
+                var closestPos = Player.Position.Extend(enemy.Position, SoldierManager.SoldierAttackRange);
+
+                if (MenuConfig.ComboR && Spells.R.IsReady() && Player.ServerPosition.Distance(enemy.ServerPosition) < Spells.R.Range)
+                    Spells.R.Cast(enemy);
+
+                if (MenuConfig.ComboW && Spells.W.IsReady())
+                    Spells.W.Cast(closestPos);
+
+                if (MenuConfig.ComboQ && Spells.Q.IsReady() && Player.ServerPosition.Distance(enemy.ServerPosition) > SoldierManager.SoldierAttackRange)
+                    Spells.Q.Cast(enemy);
+            }
+        }
+
+        static void AllInCombo()
+        {
+            var target = TargetSelector.GetTarget(Spells.Q.Range, TargetSelector.DamageType.Magical);
+
+            foreach (var enemy in HeroManager.Enemies.Where(x => x.IsValidTarget(Spells.Q.Range - 15)))
+            {
+                var closestPos = Player.Position.Extend(enemy.Position, SoldierManager.SoldierAttackRange);
+
+                if (Spells.R.IsReady() && Player.ServerPosition.Distance(enemy.ServerPosition) < Spells.R.Range)
+                    Spells.R.Cast(enemy);
+
+                if (Spells.W.IsReady())
+                    Spells.W.Cast(closestPos);
+
+                if (Spells.Q.IsReady() && Player.ServerPosition.Distance(enemy.ServerPosition) > SoldierManager.SoldierAttackRange)
+                    Spells.Q.Cast(enemy);
+
+                if (Spells.E.IsReady())
+                    Spells.E.Cast(Game.CursorPos);
+            }
+        }
+
+        static void EliteAzirCombo(Vector3 pos)
+        {
+            var backCast = Vector2.Zero;
+            backCast = Player.ServerPosition.To2D();
+
+            Spells.W.Cast(Player.ServerPosition.To2D().Extend(pos.To2D(), Spells.W.Range));
+            Spells.E.Cast(Game.CursorPos);
+
+            Utility.DelayAction.Add(
+                34,
+                () =>
+                {
+                    Spells.R.Cast(Player.ServerPosition.To2D().Extend(pos.To2D(), Player.Distance(backCast)));
+                }
+            );
+
+            Utility.DelayAction.Add(
+                425,
+                () =>
+                {
+                    Spells.Q.Cast(Player.ServerPosition.To2D().Extend(pos.To2D(), Player.Distance(backCast)));
+                }
+            );
+
+            MenuConfig.config.Item("EliteCombo").SetValue(new KeyBind('G', KeyBindType.Press, false));
+        }
+
+        static void Harass()
+        {
+            if (Player.ManaPercent < MenuConfig.HarassMana)
+                return; 
+
+            foreach (var enemy in HeroManager.Enemies.Where(x => x.IsValidTarget(Spells.Q.Range)))
+            {
+                var closestPos = Player.Position.Extend(enemy.Position, SoldierManager.SoldierAttackRange);
+
+                if (MenuConfig.HarassW && Spells.W.IsReady())
+                Spells.W.Cast(closestPos);
+
+                if (MenuConfig.HarassQ && Spells.W.IsReady() && Player.ServerPosition.Distance(enemy.ServerPosition) > Spells.W.Range)
+                    Spells.Q.Cast(enemy);
+            }
+        }
+
+        public static void Laneclear()
+        {
+            var Minions = MinionManager.GetMinions(Player.ServerPosition, Spells.W.Range);
+
+            if (Minions.Count > 0)
+            {
+                if (MenuConfig.LaneW && Spells.W.Instance.Ammo > 0 && (Minions.Count > 2 || Minions[0].Team == GameObjectTeam.Neutral))
+                {
+                    var position = Player.ServerPosition.To2D().Extend(Minions[0].Position.To2D(), Spells.W.Range);
+                    Spells.W.Cast(position);
+                }
+            }
+        }
+
+        static void Killsteal()
+        {
+            if (!Spells.Q.IsReady() || (SoldierManager.ActiveSoldiers.Count >= 0 && !Spells.W.IsReady()))
+                return;
+
+            foreach (Obj_AI_Hero target in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsValidTarget(Spells.Q.Range + 100) && 
+                !x.HasBuffOfType(BuffType.Invulnerability)).OrderByDescending(p => ComboDamage(p)))
+            {
+                if ((Player.GetSpellDamage(target, SpellSlot.Q)) > target.Health + 20)
+                {
+                    if (SoldierManager.ActiveSoldiers.Count >= 0)
+                        Spells.W.Cast(Player.Position.To2D().Extend(target.Position.To2D(), 450));
+                    else
+                        Spells.Q.Cast(target);
+                }
+            }
+        }
+        
+        static void Interrupter2_OnInterruptableTarget(Obj_AI_Hero sender, Interrupter2.InterruptableTargetEventArgs args)
+        {
+            if (args.DangerLevel != Interrupter2.DangerLevel.High)
+                return;
+
+            if (IsActive("Interrupt.R.Use") && Spells.R.IsReady())
+            {
+                var distance = Player.Distance(sender, true);
+
+                if (distance > Spells.R.RangeSqr)
+                {
+                    Spells.R.Cast(sender, false, true);
+                    return;
+                }
+
+                if (distance < Math.Pow(Math.Sqrt(Spells.R.RangeSqr + Math.Pow(Spells.R.Width + sender.BoundingRadius, 2)) , 2))
+                {
+                    var angle = (float)Math.Atan(Spells.R.Width + sender.BoundingRadius / Spells.R.Range);
+                    var position = (sender.ServerPosition.To2D() - Player.ServerPosition.To2D()).Rotated(angle);
+
+                    Spells.R.Cast(position);
+                }
+            }
+        }
+
+        static void Escape(Vector3 pos)
+        {
+            var extended = Player.ServerPosition.Extend(Game.CursorPos, 1800f);
+            
+            Spells.W.Cast(Game.CursorPos);
+            Spells.E.Cast(Game.CursorPos);
+            Spells.Q.Cast(extended);
+        }
+
+        static float ComboDamage(Obj_AI_Base target)
+        {
+            var damage = 0d;
+
+            if (Spells.Q.IsReady())
+                damage += Player.GetSpellDamage(target, SpellSlot.Q);
+
+            damage += SoldierManager.ActiveSoldiers.Count * Player.GetSpellDamage(target, SpellSlot.W);
+
+            if (Spells.E.IsReady())
+                damage += Player.GetSpellDamage(target, SpellSlot.E);
+
+            if (Spells.R.IsReady())
+                damage += Player.GetSpellDamage(target, SpellSlot.R);
+
+            if (Spells.Ignite != SpellSlot.Unknown && Player.Spellbook.GetSpell(Spells.Ignite).State == SpellState.Ready)
+                damage += Player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite);
+
+            return (float)damage;
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            if (MenuConfig.DrawQ)
+                Render.Circle.DrawCircle(Player.Position, Spells.Q.Range, MenuConfig.config.SubMenu("Drawing").Item("Draw.Q").GetValue<Circle>().Color);
+            if (MenuConfig.DrawW)
+                Render.Circle.DrawCircle(Player.Position, Spells.Q.Range, MenuConfig.config.SubMenu("Drawing").Item("Draw.W").GetValue<Circle>().Color);
+            if (MenuConfig.DrawE)
+                Render.Circle.DrawCircle(Player.Position, Spells.Q.Range, MenuConfig.config.SubMenu("Drawing").Item("Draw.E").GetValue<Circle>().Color);
+            if (MenuConfig.DrawR)
+                Render.Circle.DrawCircle(Player.Position, Spells.Q.Range, MenuConfig.config.SubMenu("Drawing").Item("Draw.R").GetValue<Circle>().Color);
+        }
+    }
+}
---
+++ Azir/MenuConfig.cs
@@ -0,0 +1,134 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using Color = System.Drawing.Color;
+
+namespace Azir
+{
+    class MenuConfig
+    {
+        public static Menu config;
+
+        public static string menuName = "Azir # By Veto";
+
+
+        private static Menu TargetSelectorMenu;
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        public static void LoadMenu()
+        {
+            config = new Menu(menuName, menuName, true);
+
+            TargetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            TargetSelector.AddToMenu(TargetSelectorMenu);
+            config.AddSubMenu(TargetSelectorMenu);
+
+
+            Orbwalker = new Orbwalking.Orbwalker(config.SubMenu("Orbwalker"));
+            config.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+
+
+            var comboMenu = new Menu("Combo", "# Combo");
+            {
+                comboMenu.AddItem(new MenuItem("Combo.Q.Use", "Use Q")).SetValue(true);
+                comboMenu.AddItem(new MenuItem("Combo.Q.MaxRng", "Use Q Max Range")).SetValue(true);
+                comboMenu.AddItem(new MenuItem("Combo.W.Use", "Use W")).SetValue(true);
+                comboMenu.AddItem(new MenuItem("Combo.E.Use", "Use E")).SetValue(false);
+                comboMenu.AddItem(new MenuItem("Combo.R.Use", "Use R")).SetValue(true);
+                comboMenu.AddItem(new MenuItem("--", "- Extras"));
+                comboMenu.AddItem(new MenuItem("Combo.All_In", "All-In")).SetValue(new KeyBind('A', KeyBindType.Press));
+                comboMenu.AddItem(new MenuItem("Special", "Special Feature"));
+                comboMenu.AddItem(new MenuItem("EliteCombo", "Elite Combo")).SetValue(new KeyBind('G', KeyBindType.Press));
+                comboMenu.AddItem(new MenuItem("Dont", "Don't Change Elite Key"));
+
+                config.AddSubMenu(comboMenu);
+            }
+
+            var harassMenu = new Menu("Harass", "# Harass");
+            {
+                harassMenu.AddItem(new MenuItem("Harass.Q.Use", "Use Q")).SetValue(true);
+                harassMenu.AddItem(new MenuItem("Harass.W.Use", "Use W")).SetValue(true);
+                harassMenu.AddItem(new MenuItem("Harass.Mana", "Minimum Mana %")).SetValue(new Slider(40, 1, 99));
+
+                config.AddSubMenu(harassMenu);
+            }
+
+            var laneMenu = new Menu("Lane", "# Lane/Jungle Clear");
+            {
+                laneMenu.AddItem(new MenuItem("Lane.Q.Use", "Use Q")).SetValue(false);
+                laneMenu.AddItem(new MenuItem("Lane.W.Use", "Use W")).SetValue(true);
+                laneMenu.AddItem(new MenuItem("Lane.Min.Minions", "Minions to Summon Soldier")).SetValue(new Slider(3, 1, 6));
+                laneMenu.AddItem(new MenuItem("Lane.Mana", "Minimum Mana %")).SetValue(new Slider(40, 1, 99));
+
+                config.AddSubMenu(laneMenu);
+            }
+
+            var killstealMenu = new Menu("Killsteal", "# Killsteal");
+            {
+                killstealMenu.AddItem(new MenuItem("Killsteal.Q.Use", "Use Q")).SetValue(true);
+            }
+
+            var interruptMenu = new Menu("Interrupt", "# Interrupt");
+            {
+                interruptMenu.AddItem(new MenuItem("Interrupt.R.Use", "Use R")).SetValue(false);
+
+                config.AddSubMenu(interruptMenu);
+            }
+
+            var miscMenu = new Menu("Misc", "# Misc");
+            {
+                miscMenu.AddItem(new MenuItem("Escape", "Flee")).SetValue(new KeyBind('Z', KeyBindType.Press));
+
+                config.AddSubMenu(miscMenu);
+            }
+
+            var drawMenu = new Menu("Drawing", "# Drawings");
+            {
+                drawMenu.AddItem(new MenuItem("DrawDamage", "Draw Damage")).SetValue(true);
+
+                drawMenu.AddItem(new MenuItem("Draw.Q", "Draw Q")).SetValue(new Circle(true, Color.Blue));
+                drawMenu.AddItem(new MenuItem("Draw.W", "Draw W")).SetValue(new Circle(true, Color.Blue));
+                drawMenu.AddItem(new MenuItem("Draw.E", "Draw E")).SetValue(new Circle(true, Color.Blue));
+                drawMenu.AddItem(new MenuItem("Draw.R", "Draw R")).SetValue(new Circle(true, Color.Blue));
+
+                config.AddSubMenu(drawMenu);
+            }
+
+            config.AddItem(new MenuItem("Author", "Author: Veto"));
+            config.AddItem(new MenuItem("Beta", "[BETA] Expect Bugs"));
+
+            config.AddToMainMenu();
+        }
+
+        // Combo
+        public static bool ComboQ { get { return config.Item("Combo.Q.Use").GetValue<bool>(); } }
+        public static bool ComboQMaxRange { get { return config.Item("Combo.Q.MaxRng").GetValue<bool>(); } }
+        public static bool ComboW { get { return config.Item("Combo.W.Use").GetValue<bool>(); } }
+        public static bool ComboE { get { return config.Item("Combo.E.Use").GetValue<bool>(); } }
+        public static bool ComboR { get { return config.Item("Combo.R.Use").GetValue<bool>(); } }
+        public static bool ComboAllIn { get { return config.Item("Combo.All_In").GetValue<bool>(); } }
+        public static bool ComboElite { get { return config.Item("EliteCombo").GetValue<bool>(); } }
+
+        // Harass
+        public static bool HarassQ { get { return config.Item("Harass.Q.Use").GetValue<bool>(); } }
+        public static bool HarassW { get { return config.Item("Harass.W.Use").GetValue<bool>(); } }
+        public static int HarassMana { get { return config.Item("Harass.Mana").GetValue<Slider>().Value; } }
+
+        // Lane / Jungle Clear
+        public static bool LaneQ { get { return config.Item("Lane.Q.Use").GetValue<bool>(); } }
+        public static bool LaneW { get { return config.Item("Lane.W.Use").GetValue<bool>(); } }
+        public static int LaneMinions { get { return config.Item("Lane.Min.Minions").GetValue<Slider>().Value; } }
+        public static int LaneMana { get { return config.Item("Lane.Mana").GetValue<Slider>().Value; } }
+
+        // Killsteal
+        public static bool KillstealQ { get { return config.Item("Killsteal.Q.Use").GetValue<bool>(); } }
+
+        // Interrupt
+        public static bool InterruptR { get { return config.Item("Interrupt.R.Use").GetValue<bool>(); } }
+
+        // Drawings
+        public static bool DrawQ { get { return config.Item("Interrupt.Q").GetValue<bool>(); } }
+        public static bool DrawW { get { return config.Item("Draw.W").GetValue<bool>(); } }
+        public static bool DrawE { get { return config.Item("Draw.E").GetValue<bool>(); } }
+        public static bool DrawR { get { return config.Item("Draw.R").GetValue<bool>(); } }
+    }
+}
